02e1b8a00e76f213b6d01ed41a88824c0cea4f98
unknown <galaxy.net0710@gmail.com>
2019-06-04 18:12:22 -0500

    Updates

---
+++ precache-manifest.cc5741a5f6b313f530eab7989083d369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd65685ac45918f9270",
+    "url": "/Mapas-Gr-ficas-React.JS/static/css/main.6f0cf6aa.chunk.css"
+  },
+  {
+    "revision": "8dd65685ac45918f9270",
+    "url": "/Mapas-Gr-ficas-React.JS/static/js/main.5e8b0c5d.chunk.js"
+  },
+  {
+    "revision": "d7bbf3878125594179ab",
+    "url": "/Mapas-Gr-ficas-React.JS/static/js/runtime~main.fcf79122.js"
+  },
+  {
+    "revision": "dad988288597d2ce93a2",
+    "url": "/Mapas-Gr-ficas-React.JS/static/css/2.59641c9c.chunk.css"
+  },
+  {
+    "revision": "dad988288597d2ce93a2",
+    "url": "/Mapas-Gr-ficas-React.JS/static/js/2.5f0016c6.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mapas-Gr-ficas-React.JS/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "06e01942b8e2d2a917bbd07eb106850d",
+    "url": "/Mapas-Gr-ficas-React.JS/index.html"
+  }
+];
